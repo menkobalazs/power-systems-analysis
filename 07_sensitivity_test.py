0507642795eb7f0c5e30f861be870c0fcae78d50
menkobalazs <menkobalazs1@gmail.com>
2026-06-02 13:37:15 +0200

    extend argparser + exclude previous runs

--- 07_sensitivity_test.py
+++ 07_sensitivity_test.py
@@ -8,31 +8,34 @@ import numpy as np
 import pandas as pd
 import argparse
 import os
+from glob import glob
 print('--- Packages imported. ---')
 
 ###########################################################
 
-parser = argparse.ArgumentParser(description="Run PyPSA optimization in a loop.")
-parser.add_argument('-f', "--function", type=str, choices=['lin', 'log'], default='log',
-                    help="*")
-parser.add_argument('--log10-limits', type=bool, choices=[True, False], default=True,
-                    help="*")
+parser = argparse.ArgumentParser(description="Run sensitivity analysis by scaling cost parameters across a range of multipliers.")
+parser.add_argument('-f', "--function", type=str, choices=['lin', 'log'], required=True,
+                    help="Spacing function for cost multipliers: 'lin' for linear, 'log' for logarithmic.")
 parser.add_argument('-l', "--lower_limit", type=float, default=0.001,
-                    help="*")
+                    help="Lower bound of the cost multiplier range.")
 parser.add_argument('-u', "--upper_limit", type=float, default=1000,
-                    help="*")
+                    help="Upper bound of the cost multiplier range.")
+parser.add_argument('-g', '--set_limit_log_scale', type=bool, default=False, action=argparse.BooleanOptionalAction,
+                    help="Interpret lower/upper limits as log10 values when using log spacing.")
+parser.add_argument('-e', '--set_limit_exp_scale', type=bool, default=False, action=argparse.BooleanOptionalAction,
+                    help="Interpret lower/upper limits as exponents (10^x) when using linear spacing.")
 parser.add_argument('-n', "--num_of_optimization", type=int, default=41,
-                    help="*")
+                    help="Number of optimization runs (points in the multiplier range).")
 parser.add_argument('-c', "--changed_cost_params", nargs="+", default=cost_params,
-                    help="*")
+                    help="Cost parameters to vary (e.g. investment, fixedom, fuel).")
 parser.add_argument('-t', "--changed_technologies", nargs="+", default=list(tech_colors.keys())[:-2],
-                    help="*")
+                    help="Technologies whose costs are modified.")
 parser.add_argument('-s', "--save_path", type=str, default='data/sensitivity_test/networks/',
-                    help="*")
-parser.add_argument('-b', "--create_baseline", type=bool, default=False,
-                    help="*")
+                    help="Directory to save resulting network files.")
+parser.add_argument('-b', "--create_baseline", type=bool, default=False, action=argparse.BooleanOptionalAction,
+                    help="Create a baseline optimization run before the sensitivity scans.")
 parser.add_argument("--baseline_name", type=str, default='baseline_1',
-                    help="*")
+                    help="Name for the baseline network file.")
 args = parser.parse_args()
 
 ###########################################################
@@ -73,13 +76,20 @@ if args.create_baseline:
 
 print('--- Start optimizations. ---')
 f_space = np.linspace if args.function == 'lin' else np.logspace
-if args.log10_limits and args.function == 'log':
+if args.function == 'log' and args.set_limit_log_scale:
     args.lower_limit, args.upper_limit = np.log10(args.lower_limit), np.log10(args.upper_limit)
+if args.function == 'lin' and args.set_limit_exp_scale:
+    args.lower_limit, args.upper_limit = 10**args.lower_limit, 10**args.upper_limit
 cost_multipliers = f_space(args.lower_limit, args.upper_limit, args.num_of_optimization)
-# TODO: drop close simulations with $\eps$ distance
-#cost_multipliers = cost_multipliers[~np.isclose(cost_multipliers, 1.0)] # drop 1 to avoid similar runs
+
 for cp in args.changed_cost_params:
-    for x in cost_multipliers:
+    runned_simulations = glob(args.save_path+cp+'/*')
+    existing_multipliers = [float(item.split('/')[-1].split('_')[-1].split('.nc')[0]) for item in runned_simulations]
+    # exclude multipliers that are within +/-10% of any existing multiplier
+    cost_multipliers_filtered = [x for x in cost_multipliers
+                                 if not any(abs(x - ex) / max(abs(ex), 1e-12) <= 0.1 for ex in existing_multipliers)]
+    print(f'--- {cp}: {len(cost_multipliers_filtered)} out of {len(cost_multipliers)} multipliers will be simulated. ---')
+    for x in cost_multipliers_filtered:
         costs_generator = pd.read_excel(data_path, sheet_name='costs_generator', index_col=0, na_values='None').to_dict()
         change_costs(costs_generator, technologies=args.changed_technologies, cost_params=[cp], allow_rand_seed=False, function='constant', x=x)
         network = build_and_optimize_network(cp+'_'+str(np.round(x,5)), costs_generator, costs_storage, dates, demand, 
